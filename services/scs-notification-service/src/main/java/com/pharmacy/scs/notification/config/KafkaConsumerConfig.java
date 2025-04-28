7e0b3ccc48cd9d1f17875f1ebf35fc3a09991d30
Bratchykau Uladzislau <kinyhaha@gmail.com>
2025-04-28 17:06:44 +0300

    feature/kafka_service  add new service

---
+++ services/scs-notification-service/src/main/java/com/pharmacy/scs/notification/config/KafkaConsumerConfig.java
@@ -0,0 +1,4 @@
+package com.pharmacy.scs.notification.config;
+
+public class KafkaConsumerConfig {
+}
---
+++ services/scs-notification-service/src/test/java/com/pharmacy/scs/notification/service/KafkaListenerIntegrationTest.java
@@ -0,0 +1,4 @@
+package com.pharmacy.scs.notification.service;
+
+public class KafkaListenerIntegrationTest {
+}
